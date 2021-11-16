d7d806f53fd5c53a7702cd0429add0bc7081d798
f0t1h <--global>
2021-11-16 10:37:57 -0800

    Moved reverse complement to its header

--- src/main.cpp
+++ src/main.cpp
@@ -32,11 +32,13 @@
 #include "IITree.h"
 #include <zlib.h>
 #include "kekseq.h"
+#include "reverse_complement.h"
 
 #ifdef DEBUG
     using std::cerr;
     std::cerr << "DEBUG MODE!\n";
 #else
+
 class mockstream{
     template<class K>
     friend mockstream& operator << (mockstream &mock, const K &k){
@@ -55,76 +57,7 @@ using std::set;
 
 std::mt19937 rand_gen{std::random_device{}()};
 
-namespace reverse_complement{
-    template<char C>
-    struct complement{
-        static constexpr char value = C;
-    };
-    //Specializations for the bases
-    #define complement_case(X,Y)            \
-        template<>                          \
-        struct complement<X>{               \
-            static constexpr char value = Y;\
-        }
-    complement_case('A','T');
-    complement_case('T','A');
-    complement_case('G','C');
-    complement_case('C','G');
-    complement_case('a','t');
-    complement_case('t','a');
-    complement_case('g','c');
-    complement_case('c','g');
-    complement_case('U','A');
-    complement_case('u','a');
-    #undef complement_case
 
-    //Lookup table class, inherits array, specialized constructor to make lookuptable compile time
-    template< size_t N = 127>
-    class complement_lookup : public std::array<char, N>{
-        public:
-        template<char... args>
-        constexpr complement_lookup (std::integer_sequence<char, args...> const&) : 
-        std::array<char,N> {complement<args>::value...}
-        {}
-    };
-    //Builds lookup table
-    template<size_t N = 127>
-    constexpr auto make_complement_table(){
-        return complement_lookup<N>{std::make_integer_sequence<char, N> {}};
-    }
-#ifdef DEBUG
-    //This wouldn't compile if make_complement_table is not computed during compilation
-    static_assert(make_complement_table() == make_complement_table());
-#endif
-    void complement_inplace(string &seq){
-        static constexpr complement_lookup table = make_complement_table();    
-        std::reverse(seq.begin(), seq.end());
-        for( char &c : seq){                 
-            c = table[c];          
-        }                                    
-    }
-    //Inheriting string to add specialized complement constructor
-    class complement_string: public string{
-        static constexpr complement_lookup table = make_complement_table();    
-        public:
-            template<class InputIt>
-            constexpr complement_string(InputIt begin, InputIt end){
-                reserve(end-begin);
-                for(InputIt it = begin; it != end; ++it){
-                    push_back(table[*it]);
-                }
-            }
-            complement_string(const string &seq){
-                reserve(seq.crend()-seq.crbegin());
-                for(auto it = seq.crbegin(); it != seq.crend(); ++it){
-                    push_back(table[*it]);
-                }
-            }
-    };
-    string complement_seq(const string &seq){
-        return complement_string{seq.rbegin(), seq.rend()};                                    
-    }
-}
 
 vector<string> rsplit(string str, string delim){
     std::vector<std::string> splits;
