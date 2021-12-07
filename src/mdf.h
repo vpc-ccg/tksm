41dce56bf8e45f320b55b3cb975898e66b8a7ea5
f0t1h <--global>
2021-12-07 11:05:06 -0800

    Added mdf.h

--- src/mdf.h
+++ src/mdf.h
@@ -10,6 +10,48 @@
 using std::vector;
 using std::string;
 using std::ostream;
+using std::ifstream;
+
+vector<pcr_copy> parse_mdf(ifstream &ist){
+
+    vector<pcr_copy> molecules;
+    string buffer;
+    buffer.reserve(1000);
+    while(std::getline(ist, buffer)){
+        auto fields = rsplit(buffer, "\t");
+        string id {fields[0].substr(1)};
+        int depth{stoi(fields[1])};
+        int exon_count{stoi(fields[2])};
+        string comment{fields[3]};
+        vector<ginterval> segments;
+        vector<std::pair<int, char>> errors_so_far;
+        size_t size_so_far = 0;
+        for(int i = 0; i< exon_count; ++i){
+            std::getline(ist, buffer);
+            auto fields = rsplit(buffer, "\t");
+            string chr{fields[0]};
+            int start {stoi(fields[1])};
+            int end   {stoi(fields[2])};
+            string strand{fields[3]};
+            if(fields.size() > 4){
+                auto mutations = rsplit(fields[4], ",");
+                for(string mutation : mutations){
+                    if(mutation == ""){
+                        continue;
+                    }
+                    char target = mutation[mutation.size()-1];
+                    int mutation_pos = stoi(mutation.substr(0,mutation.size()-1));
+                    errors_so_far.push_back(std::make_pair( size_so_far + mutation_pos, target));
+                }
+            }
+            size_so_far += (end-start);
+            segments.emplace_back(chr, start, end, strand);
+        }
+        molecules.emplace_back(id,segments, errors_so_far, depth);
+    }
+    return molecules;
+}
+
 void print_mdf(ostream &ost, const string &id, const pcr_molecule &molecule, const vector< vector<std::pair<int, char>>> &errors_per_segment){
 
     size_t interval_count = 0;//molecule.paired.size();
