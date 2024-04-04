f59390aa2c28e9af16ec92695279d609d4165bbf
f0t1h <fatihkaraoglanoglu@gmail.com>
2024-04-04 12:32:34 -0700

    extern the array length for python runner (fixes ub false positive)

--- src/python_runner.h
+++ src/python_runner.h
@@ -1,6 +1,8 @@
 #ifndef __PYTHON_RUNNER_H__
 #define __PYTHON_RUNNER_H__
 #define PY_SSIZE_T_CLEAN
+#include <string.h>
+
 #include <map>
 #include <string>
 using std::map;
@@ -41,35 +43,37 @@ public:
 
 #define NO_MODULES \
     ModuleDict {}
-#define MAKE_PYTHON_RUNNER(SCOPE, FUNC_NAME, ARRAY_NAME, MODULE_MAP)                 \
-    SCOPE int FUNC_NAME(int argc, char **argv_s) {  \
-        char  program_string[ARRAY_LENGTH(ARRAY_NAME) + 1] = {0};               \
-        strncpy(program_string, reinterpret_cast<const char *>(ARRAY_NAME), ARRAY_LENGTH(ARRAY_NAME));\
-        wchar_t **argv = new wchar_t *[argc];                                        \
-        for (int i = 0; i < argc; i++) argv[i] = (wchar_t *)GetWC(argv_s[i]);        \
-        wchar_t *program = Py_DecodeLocale("", NULL);                                \
-        if (program == NULL) {                                                       \
-            fprintf(stderr, "Fatal error: cannot decode argv[0]");                   \
-            exit(1);                                                                 \
-        }                                                                            \
-        Py_SetProgramName(program);                                                  \
-        Py_Initialize();                                                             \
-        for (const auto module_pair : MODULE_MAP) {                                  \
-            PyObject *module     = PyImport_AddModule(module_pair.first.c_str());    \
-            PyObject *moduleDict = PyModule_GetDict(module);                         \
-            PyRun_String(module_pair.second, Py_file_input, moduleDict, moduleDict); \
-        }                                                                            \
-        PySys_SetArgvEx(argc, argv, 0);                                              \
-        if (PyRun_SimpleString(program_string) != 0) {   \
-            PyErr_Print();                                                           \
-            return 1;                                                                \
-        }                                                                            \
-        if (Py_FinalizeEx() < 0) {                                                   \
-            return 120;                                                              \
-        }                                                                            \
-        PyMem_RawFree(program);                                                      \
-        return 0;                                                                    \
-    }                                                                                \
+#define MAKE_PYTHON_RUNNER(SCOPE, FUNC_NAME, ARRAY_NAME, MODULE_MAP)                                   \
+    SCOPE int FUNC_NAME(int argc, char **argv_s) {                                                     \
+        extern unsigned int ARRAY_LENGTH(ARRAY_NAME);                                                  \
+        char program_string[ARRAY_LENGTH(ARRAY_NAME) + 1];                                             \
+        strncpy(program_string, reinterpret_cast<const char *>(ARRAY_NAME), ARRAY_LENGTH(ARRAY_NAME)); \
+        program_string[ARRAY_LENGTH(ARRAY_NAME)] = 0;                                                  \
+        wchar_t **argv                           = new wchar_t *[argc];                                \
+        for (int i = 0; i < argc; i++) argv[i] = (wchar_t *)GetWC(argv_s[i]);                          \
+        wchar_t *program = Py_DecodeLocale("", NULL);                                                  \
+        if (program == NULL) {                                                                         \
+            fprintf(stderr, "Fatal error: cannot decode argv[0]");                                     \
+            exit(1);                                                                                   \
+        }                                                                                              \
+        Py_SetProgramName(program);                                                                    \
+        Py_Initialize();                                                                               \
+        for (const auto module_pair : MODULE_MAP) {                                                    \
+            PyObject *module     = PyImport_AddModule(module_pair.first.c_str());                      \
+            PyObject *moduleDict = PyModule_GetDict(module);                                           \
+            PyRun_String(module_pair.second, Py_file_input, moduleDict, moduleDict);                   \
+        }                                                                                              \
+        PySys_SetArgvEx(argc, argv, 0);                                                                \
+        if (PyRun_SimpleString(program_string) != 0) {                                                 \
+            PyErr_Print();                                                                             \
+            return 1;                                                                                  \
+        }                                                                                              \
+        if (Py_FinalizeEx() < 0) {                                                                     \
+            return 120;                                                                                \
+        }                                                                                              \
+        PyMem_RawFree(program);                                                                        \
+        return 0;                                                                                      \
+    }                                                                                                  \
     static_assert(true, "")
 
 #define MAKE_PYTHON_RUNNER_NO_MODULES(SCOPE, FUNC_NAME, ARRAY_NAME) \
